54d0aa9f850c3caea40df7e184499e64c58fe3a6
feza906 <fatimafeza04@gmail.com>
2023-01-24 20:47:25 +1100

    image slider added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import laptop from '../assets/1.png'
+import phone from '../assets/2.png'
+import Slider from 'react-slick'
+import { ImageSliders } from '../components/ImageSliders'
+
+
 
 export const Home = () => {
+  const slides = [{src:laptop},{src:phone}];
+  const containerStyles = {
+    width: '100%',
+    height:'280px'
+  }
   return (
-    <div>Home</div>
+    <div>
+      <div style={containerStyles}>
+      <ImageSliders slides={slides}/>
+      </div>
+      <div className='text-center mt-12 uppercase text-xl font-semibold'>Top Deals</div>
+    </div>
   )
 }
