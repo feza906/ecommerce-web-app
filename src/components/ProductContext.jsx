7902a3f8d61b3889d0927fa4baa3c497a4f31342
feza906 <fatimafeza04@gmail.com>
2023-01-27 12:15:39 +1100

    featured products added

--- src/components/ProductContext.jsx
+++ src/components/ProductContext.jsx
@@ -1,1 +1,33 @@
-import {createContext,useContext,useEffect} from 'react';
+import axios from 'axios';
+import {createContext,useContext,useEffect, useState} from 'react';
+
+
+const AppContext = createContext();
+const API = 'https://api.pujakaitem.com/api/products';
+
+
+const AppProvider = ({children}) => {
+    const [featureProduct,setFeatureProduct] = useState([]);
+    const isFeatured = (value) => {
+        return value.featured === true;
+    }
+    const getFeatureProducts = async (url) => {
+        const res = await axios.get(url);
+        const products = await res.data;
+        console.log(products);
+        const Feature = products.filter(isFeatured);
+        console.log(Feature);
+        setFeatureProduct(Feature);
+    }
+    useEffect(() => {
+        getFeatureProducts(API);
+    },[])
+return <AppContext.Provider value={featureProduct}>
+    {children}
+</AppContext.Provider>
+}
+const useGlobal = () => {
+    return useContext(AppContext)
+}
+
+export {AppProvider,AppContext,useGlobal};
